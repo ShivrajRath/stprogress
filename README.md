7aa7499c5a7f2b9de79dd370c2baaad1368f092d
Shivraj Rath <shivrajrath@gmail.com>
2015-07-12 10:35:41 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# stProgress.js
+
+[http://novicelab.org/stprogress](http://novicelab.org/stprogress)
+
+A light-weighted slim top progress bar to indicate service calls happening from your app. This is particularly useful for AJAX heavy applications. Just include the library and it takes care of the rest.
+
+### Install
+``npm install stprogress``
+
+``bower install stprogress``
+
+### Features
+No Configuration needed
+
+Automatically displays if AJAX calls are in background
+
+Runs with any framework
+
+AMD/CommonJS Support
+
+1kb minified
+
+### Demo
+
+[http://novicelab.org/stprogress](http://novicelab.org/stprogress)
+
+### Reference
+1. [NProgress] (https://github.com/rstacruz/nprogress)
+1. [lodash](https://github.com/lodash/lodash/blob/master/lodash.js) for module pattern.
---
+++ src/stprogress.js
@@ -0,0 +1,181 @@
+/* stProgress, (c) 2015 Shivraj Rath - http://novicelab.org/stprogress
+ * @license MIT
+ */
+;(function (XHR) {
+
+  'use strict';
+
+  /** Used to determine if values are of the language type `Object`. */
+  var objectTypes = {
+    'function': true,
+    'object': true
+  };
+
+  /** Detect free variable `exports`. */
+  var freeExports = objectTypes[typeof exports] && exports && !exports.nodeType && exports;
+
+  /** Detect free variable `module`. */
+  var freeModule = objectTypes[typeof module] && module && !module.nodeType && module;
+
+  /** Detect free variable `global` from Node.js. */
+  var freeGlobal = freeExports && freeModule && typeof global == 'object' && global && global.Object && global;
+
+  /** Detect free variable `self`. */
+  var freeSelf = objectTypes[typeof self] && self && self.Object && self;
+
+  /** Detect free variable `window`. */
+  var freeWindow = objectTypes[typeof window] && window && window.Object && window;
+
+  /** Detect the popular CommonJS extension `module.exports`. */
+  var moduleExports = freeModule && freeModule.exports === freeExports && freeExports;
+
+  /**
+   * Used as a reference to the global object.
+   *
+   * The `this` value is used if it's the global object to avoid Greasemonkey's
+   * restricted `window` object, otherwise the `window` object is used.
+   */
+  var root = freeGlobal || ((freeWindow !== (this && this.window)) && freeWindow) || freeSelf || this;
+
+  function runInContext() {
+    // Indicates whether progress should continue or stop
+    var
+      delay = 500,
+      activeXHRs = 0,
+      continueProgress = false,
+      // Progress bar div
+      progressDiv = document.getElementById('progress');
+
+    /**
+     * Set's progress bar's width
+     * @param {Number} width Width
+     */
+    function setProgressWidth(width) {
+      progressDiv.style.width = width + '%';
+    }
+
+    /**
+     * Recursively progress the bar with regular increments
+     * @param {Number} progressed progress percentage
+     */
+    function progress(progressed) {
+      window.setTimeout(function () {
+
+        // Remaining percentage of progress
+        var remaining = 100 - progressed;
+
+        // If progress bar count exceeds 99, stop the progress; the page is super slow
+        if (progressed > 99 || !continueProgress) {
+          completeProgress();
+          return;
+        }
+
+        // Calculate the next progress width
+        progressed = progressed + (0.15 * Math.pow(1 - Math.sqrt(remaining), 2));
+
+        // Set the progress bar width
+        setProgressWidth(progressed);
+
+        // Progress with next count
+        progress(progressed);
+
+        // Increments after every half a second
+      }, delay);
+    }
+
+    /**
+     * Starts the progress bar
+     */
+    function startProgress() {
+      // If not started already
+      if (activeXHRs < 1) {
+
+        // Reset activeXHRs
+        activeXHRs = 0;
+
+        continueProgress = true;
+        // Set progress div display block
+        progressDiv.style.display = 'block';
+        // Start the progress
+        progress(0);
+      }
+
+      activeXHRs++;
+    }
+
+    /**
+     * Complete the progress
+     */
+    function completeProgress() {
+
+      activeXHRs--;
+
+      if (activeXHRs < 1) {
+        // Wait for half a second to verify a new request has not been triggered
+        window.setTimeout(function () {
+          if (activeXHRs < 1) {
+            continueProgress = false;
+            setProgressWidth(101);
+            window.setTimeout(function () {
+              progressDiv.style.display = 'none';
+              setProgressWidth(0);
+              // After half a second stop the progress
+            }, delay);
+          }
+        }, delay);
+      }
+    }
+
+    // Override XHR open and send
+    var open = XHR.prototype.open;
+    var send = XHR.prototype.send;
+
+    XHR.prototype.open = function (method, url, async, user, pass) {
+      startProgress();
+      open.call(this, method, url, async, user, pass);
+    };
+
+    XHR.prototype.send = function (data) {
+      var self = this;
+
+      function onReadyStateChange() {
+        if (self.readyState === 4) {
+          completeProgress();
+        }
+      }
+      this.addEventListener('readystatechange', onReadyStateChange, false);
+      send.call(this, data);
+    };
+  }
+
+  // Export progressbar
+  var stProgress = runInContext();
+
+  if (typeof define == 'function' && typeof define.amd == 'object' && define.amd) {
+    // Expose lodash to the global object when an AMD loader is present to avoid
+    // errors in cases where lodash is loaded by a script tag and not intended
+    // as an AMD module. See http://requirejs.org/docs/errors.html#mismatch for
+    // more details.
+    root.stProgress = stProgress;
+
+    // Define as an anonymous module so, through path mapping, it can be
+    // referenced as the "underscore" module.
+    define(function () {
+      return stProgress;
+    });
+  }
+  // Check for `exports` after `define` in case a build optimizer adds an `exports` object.
+  else if (freeExports && freeModule) {
+    // Export for Node.js or RingoJS.
+    if (moduleExports) {
+      freeModule.exports = stProgress;
+    }
+    // Export for Rhino with CommonJS support.
+    else {
+      freeExports.stProgress = stProgress;
+    }
+  } else {
+    // Export for a browser or Rhino.
+    root.stProgress = stProgress;
+  }
+}).call(this, XMLHttpRequest);
